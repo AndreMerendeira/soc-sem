6c29d064dba47a14274dcd3f3e1f472537b4b1cd
AndreMerendeira <andre.merendeira@gmail.com>
2021-02-09 13:43:55 +0000

    decreased mem size. changed functions declaration to submodule

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -1,135 +1,7 @@
 #include "stdlib.h"
 #include "system.h"
 #include "periphs.h"
-#include "iob-uart.h"
-#include "printf.h"
-#include "iob_timer.h"
-#include "irq.h"
-
-extern volatile char resp_buffer[1300];
-extern volatile unsigned int resp_end;
-
-/*
-Waits for the response from SEM Core
-Response terminated by returning to a state (signaled by "> ")
-*/
-void wait_resp () {
-
-	while (resp_end!=1) { }
-
-	uart_printf("%s",resp_buffer);
-	resp_end = 0;
-}
-
-/*
-Waits for the response from SEM Core,
-but marks the asked line with "<-----"
-*/
-void wait_resp_w_word (unsigned int word) {
-	unsigned int i, line=0;
-
-	while (resp_end!=1) { }
-
-	for (i=0; resp_buffer[i] != '\0'; i++) {
-		if(resp_buffer[i]=='\n') {
-			if (line-1==word)
-			uart_printf(" <-----");
-			line++;
-		}
-		uart_putc(resp_buffer[i]);
-	}
-	resp_end = 0;
-}
-
-/*
-Enters IDLE state and waits for response
-*/
-void idle_cmd() {
-
-	uart_puts_i(1, "I\r"); //Enter IDLE state
-
-	wait_resp();
-}
-
-/*
-Enters OBSERVATION state and waits for response
-*/
-void observation_cmd() {
-
-	idle_cmd();
-	uart_puts_i(1, "O\r"); //Enter OBSERVATION state
-
-	wait_resp();
-}
-
-/*
-Enters DETECT ONLY state and waits for response
-*/
-void detect_only_cmd() {
-
-	idle_cmd();
-	uart_puts_i(1, "D\r"); //Enter DETECT ONLY state
-
-	wait_resp();
-}
-
-/*
-Enters DIAGNOSTIC SCAN state and waits for response
-*/
-void diagnostic_cmd() {
-
-	idle_cmd();
-	uart_puts_i(1, "U\r"); //Enter DIAGNOSTIC SCAN state
-
-	wait_resp();
-}
-
-/*
-Performs a software reset on the SEM core
-The reset returns an initilization report
-*/
-void reset_cmd() {
-
-	idle_cmd();
-	uart_puts_i(1, "R 00\r"); //Send reset command
-
-	wait_resp();
-}
-
-void status_cmd() {
-
-	uart_puts_i(1, "S\r"); //Send status command
-
-	wait_resp();
-}
-
-void query_cmd (unsigned int slr, unsigned int lfa, unsigned int word, unsigned int bit) {
-	char cmd_buffer [15] = "";
-	unsigned int location = 0;
-
-	location |= bit;
-	location |= word << 5;
-	location |= lfa << 12;
-	location |= slr << 29;
-
-	uart_printf_i(1,"Q C0%08X\r", location); //Send query command
-
-	wait_resp_w_word(word);
-}
-
-void err_injection_cmd (unsigned int slr, unsigned int lfa, unsigned int word, unsigned int bit) {
-	char cmd_buffer [15] = "";
-	unsigned int location = 0;
-
-	location |= bit;
-	location |= word << 5;
-	location |= lfa << 12;
-	location |= slr << 29;
-
-	uart_printf_i(1,"N C0%08X\r", location); //Send error injection command
-
-	wait_resp();
-}
+#include "sem.h"
 
 int main()
 {
@@ -137,25 +9,26 @@ int main()
 	resp_end=0;
 	timer_init(TIMER_BASE);
 	uart_init(UART0_BASE, FREQ/BAUD);
-	uart_printf("HELLO\n");
 	uart_init_i(1, UART1_BASE, FREQ/BAUD);
 
 	reset_cmd();
-
-	idle_cmd();
 
 	status_cmd();
 
 	//slr 0, frame 0, word 61, bit 0
 	query_cmd(0,0,61,0);
 
+	//flip bit at slr 0, frame 0, word 61, bit 0
 	err_injection_cmd(0,0,61,0);
 
+	//check if bit was flipped
 	query_cmd(0,0,61,0);
 
 	observation_cmd();
 
 	wait_resp(); //Wait for response to error detection
+
+	wait_mseconds(100); //wait 100 miliseconds
 
 	uart_printf("\n\n");
 
